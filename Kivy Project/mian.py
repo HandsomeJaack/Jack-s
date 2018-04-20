12e03f2a1d1b56e5710e9eed45b9d61a85308153
Jack <doctor.dmitry99@gmail.com>
2018-04-20 10:52:32 +0300

    Интерфейс

--- Kivy Project/mian.py
+++ Kivy Project/mian.py
@@ -1,16 +1,40 @@
 from kivy.app import App
 from  kivy.uix.button import Button 
+from kivy.config import Config
+from  kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
 
-class MyApp(App):
+Window.clearcolor = (.37,.50,.69, 1)
+Config.set('graphics', 'width', '1080')
+Config.set('graphics', 'height', '920')
+
+def on_enter(instance, value):
+    print('User pressed enter in', instance)
+
+class ToneApp(App):
 	def build(self):
-		return Button(text = "This is my first button!",
-				front_size = 30,
-				on_press =self.btn_press,
-				backgrond_color = [1,0,0,1],
-				backgrond_normal = '')
 
-	def btn_press(self,instance):
-		instance.text = 'I am done!'
+		f1 = BoxLayout(orientation='vertical', padding = [50], spacing = 25)
+		f1.add_widget(TextInput(text = 'Введи сообщение для анализа', multiline = False))
+		f1.bind(on_text_validate=on_enter)
+		f1.add_widget(Button(text = "Отправить", 
+					font_size = 30, 
+					on_press = self.btn_press1,
+					background_color = [.17,.12,.69,1],
+					background_normal = ''));
+		f1.add_widget(Button(text = "Получить", 
+					font_size = 30, 
+					on_press = self.btn_press2,
+					background_color = [.17,.12,.69,1],
+					background_normal = ''));
+		return f1
+
+	def btn_press1(self, instance):
+		instance.text = "Отправлено!"
+
+	def btn_press2(self, instance):
+		instance.text = "Получено!"
 
 if __name__ == "__main__":
-	MyApp().run()
+	ToneApp().run()
