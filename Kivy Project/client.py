76347dd40ec142ac64f6538b809b9bf377f0e9b9
Jack <doctor.dmitry99@gmail.com>
2018-04-22 12:35:58 +0300

    Рабочая версия клиента

--- Kivy Project/client.py
+++ Kivy Project/client.py
@@ -1,12 +1,15 @@
-import socket
+def send_to_server(clients_input):
+	import socket
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-soc.connect(("127.0.0.1", 12345))
+	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+	soc.connect(("127.0.0.1", 12345))
 
-clients_input = input("Введите данные для обработки\n")  
-soc.send(clients_input.encode("utf8")) # we must encode the string to bytes  
-result_bytes = soc.recv(4096) # the number means how the response can be in bytes  
-print(result_bytes)
-#result_string = result_bytes.decode("utf8")
-#print(result_string)
-print("Результат полученныый с сервера {}".format(result_bytes.decode("Windows-1250")))  
+	#сlients_input = input("Введите данные для обработки\n")  
+	soc.send(clients_input.encode("utf8"))
+	result_bytes = soc.recv(4096) 
+	print(result_bytes)
+	result_string = result_bytes.decode("utf8")
+	print("Результат полученныый с сервера {}".format(result_string)) 
+	return result_string; 
+
+#send_to_server(input("Введите данные для обработки\n"))
